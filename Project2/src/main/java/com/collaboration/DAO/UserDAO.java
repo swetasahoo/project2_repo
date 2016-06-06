381db1ed238f9963b6504b6e571d638a9d8b5487
swetasahoo <sweta2478@gmail.com>
2016-06-06 20:34:53 +0530

    Revert "proj2_6_june"
    This reverts commit 66ddd1e0a7f7439a805b3ee43393065afa6c5567.

--- Project2/src/main/java/com/collaboration/DAO/UserDAO.java
+++
@@ -1,8 +0,0 @@
-package com.collaboration.DAO;
-
-import com.collaboration.model.UserRole;
-import com.collaboration.model.Users;
-
-public interface UserDAO {
-	public void save(Users o,UserRole r);
-}
